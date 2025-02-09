ea216b526cbd4aee97c4dab1772107ae7ee45648
Ranzao <by.ranzao@gmail.com>
2025-02-08 22:54:28 -0300

    fix a pantallas pequeñas scroll about

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 var mobileMenu = document.getElementById('mobile-menu');
-  var navLinks = document.querySelector('.nav-links');
+var navLinks = document.querySelector('.nav-links');
+var body = document.body; // Referencia al body
 
-  mobileMenu.addEventListener('click', function() {
+mobileMenu.addEventListener('click', function() {
     navLinks.classList.toggle('active');
-  });
+    body.classList.toggle('no-scroll'); // Bloquea/desbloquea el scroll
+});
+
+// Cerrar el menú al hacer clic en un enlace
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function() {
+        navLinks.classList.remove('active');
+        body.classList.remove('no-scroll'); // Rehabilita el scroll
+    });
+});
